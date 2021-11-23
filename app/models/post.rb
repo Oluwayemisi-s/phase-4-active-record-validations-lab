121ed2836bc325e8dc1a0f9cece9596f49ff7529
Oluwayemisi Soremekun <oluwayemisi.soremekun@gmail.com>
2021-11-22 21:52:08 -0800

    commit tree e7b3a5d6e5022a5dba77e3d13a0b342cb1ddacec

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { :in => ['Fiction', 'Non-fiction'] }
+    validates :title, inclusion: { :in => ["Won't Believe", "Secret", "Guess", "Top [number]"] }
+    
 end
